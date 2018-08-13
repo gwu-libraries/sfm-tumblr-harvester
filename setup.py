03bdee209fb87ca81c8f764b4d70194b6377a16c
Justin Littman <justinlittman@gwu.edu>
2018-08-13 17:05:02 -0400

    Bumped version to 2.0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmtumblrharvester',
-    version='2.0.1',
+    version='2.0.2',
     url='https://github.com/gwu-libraries/sfm-tumblr-harvester',
     author='Social Feed Manager',
     author_email='sfm@gwu.edu',
