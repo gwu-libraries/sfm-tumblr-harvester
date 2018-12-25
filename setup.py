33756793122069ba3587484fbe7ce10c8be1d14b
Dan Kerchner <kerchner@gwu.edu>
2018-12-25 11:23:48 -0500

    Updated versions of Python libraries

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
              'tumblr_warc_iter.py'],
     py_modules=['tumblr_harvester','tumblr_warc_iter'],
     install_requires=['sfmutils'],
-    tests_require=['mock>=1.3.0'],
+    tests_require=['mock==2.0.0'],
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
