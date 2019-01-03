3353c49fcffe85a7e383841d6a9be43a48e54f01
Laura Wrubel <lwrubel@gwu.edu>
2019-01-03 09:21:01 -0500

    Bumped version to 2.1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmtumblrharvester',
-    version='2.0.2',
+    version='2.1.0',
     url='https://github.com/gwu-libraries/sfm-tumblr-harvester',
     author='Social Feed Manager',
     author_email='sfm@gwu.edu',
