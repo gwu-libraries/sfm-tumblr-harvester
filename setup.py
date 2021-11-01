132659acb85322346e46fc92b3e87f0981d9bc19
Dolsy Smith <dsmith@gwu.edu>
2021-11-01 13:21:31 -0400

    bump version commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmtumblrharvester',
-    version='2.4.0',
+    version='2.5.0',
     url='https://github.com/gwu-libraries/sfm-tumblr-harvester',
     author='Social Feed Manager',
     author_email='sfm@gwu.edu',
